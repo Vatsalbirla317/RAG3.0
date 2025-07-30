4787c5485a7114fea53f3c8d1983a46bb0708d60
Vatsal Birla <vb563488@gmail.com>
2025-07-30 15:19:36 +0530

    fix: Correct tree-sitter language installation for Render

--- codematrix_backend/build.sh
+++ codematrix_backend/build.sh
@@ -2,4 +2,8 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt 
+# Step 1: Install all Python packages from requirements.txt
+pip install -r requirements.txt
+
+# Step 2: Install the tree-sitter language parsers required by LangChain
+python -c "from langchain_community.document_loaders.parsers.language.tree_sitter_parser import LanguageParser; LanguageParser.install_grammars()" 
